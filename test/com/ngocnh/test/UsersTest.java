0261587a9968a7b211b4d84e4c57a14ca7ed86cc
manhnk <nkm081198@gmail.com>
2021-01-06 21:22:04 +0700

    commitasdsadsa

---
+++ test/com/ngocnh/test/UsersTest.java
@@ -0,0 +1,27 @@
+package com.ngocnh.test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.ngocnh.app.entity.Users;
+
+public class UsersTest {
+	public static void main(String[] args) {
+		Users u = new Users();
+		u.setFullName("Ngoc1");
+		u.setEmail("ngocnh@gmail1.com");
+		u.setPassword("hello1");
+
+		EntityManagerFactory createEntityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
+		EntityManager entityManager = createEntityManagerFactory.createEntityManager();
+
+		entityManager.getTransaction().begin();
+
+		entityManager.persist(u);
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		createEntityManagerFactory.close();
+	}
+}
