20174cb91164d79a0415ca5fa5ab28b4e7e527be
manhnk <nkm081198@gmail.com>
2021-01-06 17:01:15 +0700

    first commit

---
+++ src/com/ngocnh/app/Test.java
@@ -0,0 +1,9 @@
+package com.ngocnh.app;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/ngocnh/app/entity/Users.java
@@ -0,0 +1,29 @@
+package com.ngocnh.app.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "full_name")
+	private String fullName;
+
+	@Column(name = "password")
+	private String password;
+
+}
